85c0863d9836a6ef79d65a821952adb98a0205cb
Sam Breed <sam@quickleft.com>
2015-09-04 01:12:08 -0700

    add unsplash backgrounds

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -1,5 +1,6 @@
 var ob = require('oblique-strategies');
 var version = require('../../ext/manifest.json').version;
+var setBackground = require('./unsplash-background');
 
 function renderCard() {
   var card = ob.draw();
@@ -32,11 +33,43 @@ function handleAboutClick() {
       body.classList.remove('show-about');
     }
   });
+
+  var about = document.querySelector('.about-modal');
+  about.addEventListener('click', function(e) {
+    e.preventDefault();
+    body.classList.remove('show-about');
+  }, false);
+
+  var aboutInner = document.querySelector('.about-modal .inner');
+  aboutInner.addEventListener('click', function(e) {
+    e.stopPropagation();
+  }, false);
 }
 
 function updateVersion() {
   var elem = document.getElementById('ext-version');
   elem.innerText = 'v' + version;
+}
+
+function setUnsplash() {
+  var unsplash = localStorage.getItem('unsplash');
+  var checkbox = document.querySelector('input[name="unsplash"]');
+
+  if (unsplash == true) {
+    setBackground();
+    checkbox.setAttribute('checked', true);
+  }
+
+  checkbox.addEventListener('change', function(e) {
+    var bg;
+    localStorage.setItem('unsplash', e.currentTarget.checked);
+
+    if (e.currentTarget.checked) {
+      setBackground();
+    } else if (bg = document.querySelector('.bg')) {
+      bg.remove();
+    }
+  }, true);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -49,6 +82,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }, 1000);
   renderCard();
   updateVersion();
+  setUnsplash();
   handleThemeClick();
   handleAboutClick();
 });
