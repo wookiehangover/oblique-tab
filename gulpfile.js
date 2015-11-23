db60b2fe44cc0131df404519822f5c5c0634744b
Sam Breed <sam@quickleft.com>
2015-11-22 20:11:19 -0800

    v2.4.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 'use strict'
 
-var gulp = require('gulp')
-var exec = require('child_process').exec
-var sourcemaps = require('gulp-sourcemaps')
-var postcss = require('gulp-postcss')
-var size = require('gulp-filesize')
-var smoosher = require('gulp-smoosher')
-var preprocess = require('gulp-preprocess')
-var minimist = require('minimist')
+const fs = require('fs')
+const gulp = require('gulp')
+const exec = require('child_process').exec
+const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss')
+const size = require('gulp-filesize')
+const smoosher = require('gulp-smoosher')
+const preprocess = require('gulp-preprocess')
+const minimist = require('minimist')
+const manifest = require('./ext/manifest.json')
+const pkg = require('./package.json')
 
-var allowedOptions = {
-  string: 'build',
-  default: { build: 'pages' }
-}
+const allowedOptions = [{
+  string: ['build', 'version']
+}]
 
-var options = minimist(process.argv.slice(2), allowedOptions)
+const options = Object.assign({
+  build: 'pages',
+  version: 'patch'
+}, minimist(process.argv.slice(2), allowedOptions))
 
 function run(command) {
   var child = exec(command)
@@ -28,20 +33,13 @@ function run(command) {
   })
 }
 
-function generateHTML() {
-  var buildPath = options.build === 'chrome' ? 'ext' : '.'
-
-  if (buildPath === 'chrome') {
-    console.log('Building chrome extension')
-  }
-
+function generateHTML(build) {
+  const buildPath = build === 'chrome' ? 'ext' : '.'
   return gulp.src('assets/index.html')
-    .pipe(preprocess({ context: { build: options.build } }))
+    .pipe(preprocess({ context: { build: build } }))
     .pipe(smoosher())
     .pipe(gulp.dest(buildPath))
 }
-
-gulp.task('build', ['build-chrome', 'build-pages', 'css'], generateHTML)
 
 gulp.task('build-chrome', function(done) {
   exec('npm run build-js', function(err, stdout, stderr) {
@@ -54,6 +52,41 @@ gulp.task('build-pages', function(done) {
   exec('npm run build-pages', function(err, stdout, stderr) {
     if (err) { return done(err) }
     done()
+  })
+})
+
+gulp.task('version', function updateVersion(done) {
+  const version = manifest.version.split('.')
+  let major = version[0]
+  let minor = version[1]
+  let patch = version[2]
+
+  switch (options.version) {
+    case 'major':
+      major = parseInt(major, 10) + 1 || 1
+      minor = 0
+      patch = 0
+      break
+    case 'minor':
+      minor = parseInt(minor, 10) + 1 || 0
+      patch = 0
+      break
+    case 'patch':
+    default:
+      patch = parseInt(patch, 10) + 1 || 0
+      break
+  }
+
+  manifest.version = [major, minor, patch].join('.')
+  pkg.version = manifest.version
+
+  fs.writeFile('ext/manifest.json', JSON.stringify(manifest, null, 2), function(err) {
+    if (err) throw err
+    fs.writeFile('package.json', JSON.stringify(pkg, null, 2), function(err) {
+      if (err) throw err
+      console.log(`Wrote ${options.version} version to manifest.json and package.json`)
+      done()
+    })
   })
 })
 
@@ -74,12 +107,7 @@ gulp.task('css', function() {
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('html', ['css'], generateHTML)
-
-gulp.task('release', ['build'], function(done) {
-  if (options.build !== 'chrome') {
-    return done(new Error('Not packing chrome extension. Run again with --build chrome'))
-  }
+gulp.task('release', ['pages', 'chrome', 'version'], function(done) {
   exec('find ext -name .DS_Store -delete && zip -r dist/chrome.zip ext', function(err, stdout) {
     console.log(stdout)
     done()
@@ -91,5 +119,15 @@ gulp.task('watch', function() {
   run('npm run watch-js')
   gulp.watch(['assets/css/**/*.css', 'assets/index.html'], ['html'])
 })
+
+gulp.task('html', ['css'], function() {
+  return generateHTML(options.build)
+})
+
+gulp.task('chrome', ['build-chrome', 'css'], function() {
+  return generateHTML('chrome')
+})
+
+gulp.task('pages', ['build-pages', 'html'])
 
 gulp.task('default', ['css', 'watch'])
